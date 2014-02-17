2dba973886b57881e589dbf7e1045853dad9aa6a
Bruce Snyder <bruce.snyder@hybris.com>
2014-02-17 11:11:55 -0700

    Added some string manipulation

--- helloworld.pl
+++ helloworld.pl
@@ -1,5 +1,15 @@
 #!/bin/env perl 
 
 foreach $i (1..10) {
-    print "\nHello Perl World! $i\n" 
+    print "\nHello Perl World! $i" 
 }
+
+print "\n\n";
+
+$test_str = ("This-is-a-test"); 
+@split_str = split('-', $test_str);
+$join_str = join(',', @split_str);
+
+print "Before: $test_str\n";
+print "After: $join_str\n";
+
