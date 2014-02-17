daf4019e8f4767291b2bcfc27d1e611f5042cf0e
Bruce Snyder <bruce.snyder@hybris.com>
2014-02-17 11:13:35 -0700

    Placed work into subroutines

--- helloworld.pl
+++ helloworld.pl
@@ -1,15 +1,23 @@
 #!/bin/env perl 
 
-foreach $i (1..10) {
-    print "\nHello Perl World! $i" 
+sub simple_looping {
+  foreach $i (1..10) {
+      print "\nHello Perl World! $i" 
+  }
 }
 
-print "\n\n";
+sub string_manip {
+  $test_str = ("This-is-a-test"); 
+  @split_str = split('-', $test_str);
+  $join_str = join(',', @split_str);
 
-$test_str = ("This-is-a-test"); 
-@split_str = split('-', $test_str);
-$join_str = join(',', @split_str);
+  print "Before: $test_str\n";
+  print "After: $join_str\n";
+}
 
-print "Before: $test_str\n";
-print "After: $join_str\n";
+sub main {
+  &simple_looping; 
+  &string_manip;
+}
 
+&main;
